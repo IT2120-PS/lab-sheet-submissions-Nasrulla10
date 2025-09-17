e9871137bba0bfb5d346ecbf34ea05a32a337e9c
Nasrulla M U <IT24101774@my.sliit.lk>
2025-09-17 14:04:00 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24101774.r
@@ -0,0 +1,32 @@
+
+R version 4.5.1 (2025-06-13) -- "Great Square Root"
+Copyright (C) 2025 The R Foundation for Statistical Computing
+Platform: aarch64-apple-darwin20
+
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
+
+  Natural language support but running in an English locale
+
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
+
+Type 'demo()' for some demos, 'help()' for on-line help, or
+'help.start()' for an HTML browser interface to help.
+Type 'q()' to quit R.
+
+[R.app GUI 1.82 (8536) aarch64-apple-darwin20]
+
+[History restored from /Users/mohamedrazeen/.Rapp.history]
+
+> punif(25,min=0,max=40,lower.tail=TRUE)-punif(10,min=0,max=40,lower.tail=TRUE)
+[1] 0.375
+> pexp(2,rate=1/3,lower.tail=TRUE)
+[1] 0.4865829
+> pnorm(130,mean=100,sd=15,lower.tail=FALSE)
+[1] 0.02275013
+> qnorm(0.95,mean=100,sd=15)
+[1] 124.6728
+> 
